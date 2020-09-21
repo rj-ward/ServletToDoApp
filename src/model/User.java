065d357767e8ada3724974d15b9a5ef08e1636ab
aj-ward <59932988+rj-ward@users.noreply.github.com>
2020-09-21 14:30:23 -0500

    Update User.java with basic structure.

--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,54 @@
 package model;
 
-public class User {
+import java.io.Serializable;
+
+/**
+ * Bean class for use in JSP action tags.
+ * 
+ * @author Remy Ward
+ *
+ */
+public class User implements Serializable {
+
+	/**
+	 * Default ID field
+	 */
+	private static final long serialVersionUID = 1L;
+	private String firstName; // First name of user
+	private String lastName; // Last name of user
+	private String username; // Username for account
+	private String password; // Password for account
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
