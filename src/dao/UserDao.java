d5c4b71bcf592c9b92672637f5c0eb0e74efcd1f
aj-ward <59932988+rj-ward@users.noreply.github.com>
2020-09-23 14:27:40 -0500

    Add persistance via Maven dependencies

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -1,5 +1,10 @@
 package dao;
 
-public class UserDao {
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+public class UserDao {
+	static EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("ServletToDo");
+	
+	
 }
